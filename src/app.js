e95647687fefc7ec8aec40c5240c1a6117af78c1
unknown <cmtzvitela@gmail.com>
2022-11-23 13:00:46 -0600

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Defines paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -81,6 +82,6 @@ app.get("/help/*", (req, res) => {
 app.get("*", (req, res) => {
   res.render("404", { title: "Error", name: "Andrew", error: "That page doesn't exist" });
 });
-app.listen(3000, () => {
-  console.log("Server is up on port 3000");
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
